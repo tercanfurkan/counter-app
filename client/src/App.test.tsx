c48c526d72141f0f1f6f87859b41b1aa24400b38
Furkan Tercan <tercanfurkan@gmail.com>
2023-10-11 21:44:30 +0300

    (Re)configure client test suite

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -3,8 +3,8 @@ import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import App from './App';
 
-test('it should foo', () => {
+test('it should show welcome', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/foo/i);
+  const linkElement = getByText(/Welcome/i);
   expect(linkElement).toBeInTheDocument();
 });
